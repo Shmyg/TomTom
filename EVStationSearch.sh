4276a32f617c362acd92bac05580e393440e2454
Shmyg <serge.shmygelskyy@gmail.com>
2020-01-23 13:45:47 +0200

    Initial commit

---
+++ EVStationSearch.sh
@@ -0,0 +1,1 @@
+curl -X GET "https://api.tomtom.com/search/2/nearbySearch/.xml?lat=50.45&lon=30.52&limit=50&idxSet=POI%2CPAD%2CStr%2CGeo&categorySet=7309&key=UXcDfIGHNfny0mJyPOMv44uvAkj8MUjd" -H  "accept: */*"
---
+++ PoiCategories.sh
@@ -0,0 +1,1 @@
+curl -X GET "https://api.tomtom.com/search/2/poiCategories.xml?key=UXcDfIGHNfny0mJyPOMv44uvAkj8MUjd" -H  "accept: */*"
